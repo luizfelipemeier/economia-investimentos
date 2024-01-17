b4d5ca3839118f3a34257329ca23f09d5c356bf7
luizfelipemeier <luizfelipemeier@gmail.com>
2024-01-16 22:35:31 -0300

    Sua mensagem de commit aqui

--- update_and_push.R
+++ update_and_push.R
@@ -5,12 +5,15 @@ local_lib <- file.path(Sys.getenv("GITHUB_WORKSPACE"), "R/library")
 if (!dir.exists(local_lib)) dir.create(local_lib, recursive = TRUE)
 .libPaths(c(local_lib, .libPaths()))
 
+# Carregar o token pessoal do arquivo .Renviron
+Sys.setenv(GITHUB_PAT = Sys.getenv("GITHUB_PAT"))
+
 # Instalar ou carregar os pacotes necessários
 install.packages("remotes")
 remotes::install_github("rstudio/rmarkdown")
 remotes::install_github("r-lib/usethis")
 
-# Renderize todos os arquivos R Markdown para HTML
+# Renderizar todos os arquivos R Markdown para HTML
 files <- list.files(pattern=".Rmd$", recursive=TRUE, full.names = TRUE)
 for (file in files) {
   rmarkdown::render(file)
