1c5479385f05c0136b9812e11d9764154f2334ec
luizfelipemeier <luizfelipemeier@gmail.com>
2024-01-17 00:10:46 -0300

    Sua mensagem de commit aqui

---
+++ install.R
@@ -0,0 +1,18 @@
+# install.R
+install.packages(c("rmarkdown", 
+                   "usethis", 
+                   "bookdown",
+                   "sidrar",
+                   "ggplot2",
+                   "highcharter",
+                   "plotly",
+                   "kableExtra",
+                   "DT",
+                   "knitr",
+                   "forcats",
+                   "lubridate",
+                   "GetBCBData",
+                   "dplyr",
+                   "tidyverse",
+                   "GetBCBData"
+                   ), lib=Sys.getenv("R_LIBS"))
