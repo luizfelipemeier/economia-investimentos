cb93b37e9e3a82401d9eaf82e231cc6e5ce0ca5f
luizfelipemeier <luizfelipemeier@gmail.com>
2024-01-16 22:42:29 -0300

    Sua mensagem de commit aqui

--- update_and_push.R
+++ update_and_push.R
@@ -1,16 +1,11 @@
-# update_and_push.R
-
-# Configurar o diretório de bibliotecas local
-local_lib <- file.path(Sys.getenv("GITHUB_WORKSPACE"), "R/library")
-if (!dir.exists(local_lib)) dir.create(local_lib, recursive = TRUE)
-.libPaths(c(local_lib, .libPaths()))
+# Instalar o pacote rmarkdown diretamente do CRAN
+install.packages("rmarkdown")
 
 # Carregar o token pessoal do arquivo .Renviron
 Sys.setenv(GITHUB_PAT = Sys.getenv("GITHUB_PAT"))
 
 # Instalar ou carregar os pacotes necessários
-install.packages("remotes")
-remotes::install_github("rstudio/rmarkdown")
+install.packages("usethis")
 remotes::install_github("r-lib/usethis")
 
 # Renderizar todos os arquivos R Markdown para HTML
